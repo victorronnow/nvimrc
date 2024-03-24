06dc7d2c648d98f5f4d6584cbe12ed67bfb376fd
Victor Rønnow <viktor.delaunay@gmail.com>
2024-03-24 16:50:05 +0100

    Updates

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,26 +1,27 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function ()
-      require("conform").setup({
-          formatters_by_ft = {
-              lua = { "stylua" },
-              python = { "isort", "black" },
-              javascript = { { "biome" } },
-              twig = {{ "djlint" }},
-              scss = {{ "prettierd", "prettier" }},
-              typescript = {{ "prettierd", "prettier" }},
-              php = {{ "prettierd", "prettier" }},
-              svelte = {{ "prettierd", "prettier" }},
-          },
-      })
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				twig = { { "djlint" } },
+				scss = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				-- php = { { "prettierd", "prettier" } },
+				php = { "phpcbf" },
+				svelte = { { "prettierd", "prettier" } },
+			},
+		})
 
-      vim.api.nvim_create_autocmd("BufWritePre", {
-          pattern = "*",
-          callback = function(args)
-              require("conform").format({ bufnr = args.buf })
-          end,
-      })
-  end
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
-
--- lua/config/plugins/tree.lua
+++ lua/config/plugins/tree.lua
@@ -23,15 +23,33 @@ return {
         },
     },
     opts = {
-            popup_border_style = "rounded",
-            position = 'float',
-            hide_dotfiles = false,
-            filesystem = {
-                filtered_items = {
-                    visible = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = true,
-                },
+        popup_border_style = "rounded",
+        position = 'float',
+        hide_dotfiles = false,
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+            },
+        },
+        window = {
+            mappings = {
+                ["J"] = function(state)
+                    local tree = state.tree
+                    local node = tree:get_node()
+                    local siblings = tree:get_nodes(node:get_parent_id())
+                    local renderer = require('neo-tree.ui.renderer')
+                    renderer.focus_node(state, siblings[#siblings]:get_id())
+                end,
+                ["K"] = function(state)
+                    local tree = state.tree
+                    local node = tree:get_node()
+                    local siblings = tree:get_nodes(node:get_parent_id())
+                    local renderer = require('neo-tree.ui.renderer')
+                    renderer.focus_node(state, siblings[1]:get_id())
+                end
             }
+        }
     },
 }
